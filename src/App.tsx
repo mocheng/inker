b31f20b8f7d663232bb3271f23b6b35005b67e50
Morgan Cheng <morgan.cheng@hulu.com>
2025-12-28 22:57:46 +0800

    init the project as a counter

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import { Text } from 'ink';
+
+export default function App() {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCounter(c => c + 1);
+    }, 100);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  return <Text>Counter: {counter}</Text>;
+}
